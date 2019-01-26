bf970e7c671f190e14cedd2299f3d4808cf2a1fe
dlsr <dlsr@cin.ufpe.br>
2019-01-26 11:46:53 -0300

    Initial project

---
+++ Home Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class Enemy : ScriptableObject
+{
+
+    public string name;
+    public float speed;
+    public Sprite image;
+
+    public void Walk()
+    {
+
+    }
+
+}
