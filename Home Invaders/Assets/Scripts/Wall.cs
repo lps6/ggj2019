73f0783670777cecfb726e3283dfa6a57089303f
dlsr <dlsr@cin.ufpe.br>
2019-01-27 15:27:10 -0300

    Game Over

--- Home Invaders/Assets/Scripts/Wall.cs
+++ Home Invaders/Assets/Scripts/Wall.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public LevelManager levelManager;
 
     private void OnTriggerEnter2D(Collider2D collision2)
     {
         Destroy(collision2.gameObject);
+        levelManager.EnemyDamage();
     }
 }
