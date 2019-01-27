26623ce472a847bf47ce312a2b91fb25149c3a45
dlsr <dlsr@cin.ufpe.br>
2019-01-27 16:25:29 -0300

    Current Friend Indication

--- Home Invaders/Assets/Scripts/TileManager.cs
+++ Home Invaders/Assets/Scripts/TileManager.cs
@@ -14,19 +14,36 @@ public class TileManager : MonoBehaviour
 
     private bool isClick = true;
 
+    public GameObject currentFriendScreen;
+
+    private Sprite currentFriendSprite;
+
+    private int friendRandomIndex = 0;
+
     private void Start()
     {
         isClick = true;
+        friendRandomIndex = UnityEngine.Random.Range(0, levelManager.level.availableFriends.Count);
+        currentFriendScreen.GetComponent<SpriteRenderer>().sprite = levelManager.level.availableFriends[friendRandomIndex].GetComponent<Friend>().friend.image;
     }
 
     IEnumerator ClickRoutine()
     {
+        currentFriendScreen.SetActive(false);
         yield return new WaitForSeconds(3f);
+        currentFriendScreen.SetActive(true);
+        friendRandomIndex = UnityEngine.Random.Range(0, levelManager.level.availableFriends.Count);
+        currentFriendScreen.GetComponent<SpriteRenderer>().sprite = levelManager.level.availableFriends[friendRandomIndex].GetComponent<Friend>().friend.image;
         isClick = true;
     }
 
     public void Update()
     {
+        Vector3 pos = Input.mousePosition;
+        pos.z = transform.position.z - Camera.main.transform.position.z;
+        transform.position = Camera.main.ScreenToWorldPoint(pos);
+        currentFriendScreen.transform.position = Camera.main.ScreenToWorldPoint(pos);
+
         if (Input.GetMouseButtonDown(0) && isClick)
         {
 
@@ -34,9 +51,9 @@ public class TileManager : MonoBehaviour
             Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
             if (grid.GetComponentInChildren<Tilemap>().GetTile(coordinate))
             {
-                int enemyRandomIndex = UnityEngine.Random.Range(0, levelManager.level.availableFriends.Count);
-                Instantiate(levelManager.level.availableFriends[enemyRandomIndex]).transform.position = grid.transform.GetChild(0).GetComponent<Tilemap>().GetCellCenterWorld(coordinate);
-                grid.transform.GetChild(0).GetComponent<Tilemap>().SetTile(coordinate, null);
+                //friendRandomIndex = UnityEngine.Random.Range(0, levelManager.level.availableFriends.Count);
+                Instantiate(levelManager.level.availableFriends[friendRandomIndex]).transform.position = grid.transform.GetChild(0).GetComponent<Tilemap>().GetCellCenterWorld(coordinate);
+                //grid.transform.GetChild(0).GetComponent<Tilemap>().SetTile(coordinate, null);
                 isClick = false;
                 StartCoroutine(ClickRoutine());
             }
