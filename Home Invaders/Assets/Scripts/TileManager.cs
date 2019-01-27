32dbf8940f71c8588138e0b079e2d379179ca713
dlsr <dlsr@cin.ufpe.br>
2019-01-27 15:12:10 -0300

    Friend Shoot / Click Delay

--- Home Invaders/Assets/Scripts/TileManager.cs
+++ Home Invaders/Assets/Scripts/TileManager.cs
@@ -12,23 +12,34 @@ public class TileManager : MonoBehaviour
 
     public LevelManager levelManager;
 
-    public void Start()
+    private bool isClick = true;
+
+    private void Start()
     {
-      
+        isClick = true;
     }
+
+    IEnumerator ClickRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+        isClick = true;
+    }
+
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        print(isClick);
+        if (Input.GetMouseButtonDown(0) && isClick)
         {
 
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
-            print(grid.GetComponentInChildren<Tilemap>().GetTile(coordinate));
+            //print(grid.GetComponentInChildren<Tilemap>().GetTile(coordinate));
             if (grid.GetComponentInChildren<Tilemap>().GetTile(coordinate))
             {
-                print("1");
                 Instantiate(levelManager.level.availableFriends[0]).transform.position = grid.transform.GetChild(0).GetComponent<Tilemap>().GetCellCenterWorld(coordinate);
                 grid.transform.GetChild(0).GetComponent<Tilemap>().SetTile(coordinate, null);
+                isClick = false;
+                StartCoroutine(ClickRoutine());
             }
             
             
