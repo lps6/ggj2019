3fe47892a4d901146c13fe79db383ef4016473dd
dlsr <dlsr@cin.ufpe.br>
2019-01-27 00:27:13 -0300

    Level Manager

---
+++ Home Invaders/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public LevelSO level;
+
+    void Start()
+    {
+        LoadWaves();
+    }
+
+    void LoadWaves()
+    {
+        foreach (GameObject route in level.availableRoutes)
+        {
+            StartCoroutine(LoadEnemies(Instantiate(route)));
+        }
+    }
+
+    IEnumerator LoadEnemies(GameObject route)
+    {
+        foreach (GameObject enemy in level.availableEnemies)
+        {
+            GameObject current_enemy = Instantiate(enemy);
+            current_enemy.transform.parent = route.transform;
+            yield return new WaitForSeconds(level.delay + enemy.GetComponent<Enemy>().enemy.delay);
+        }
+    }
+}
